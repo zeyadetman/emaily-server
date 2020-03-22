8a5726a1dbe28a281ceb8fca51abfb7bce25f442
Zeyad Etman <zeyadetman@gmail.com>
2020-03-22 14:32:58 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const cookieSession = require("cookie-session");
 require("./models/User");
 require("./services/passport");
 
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
