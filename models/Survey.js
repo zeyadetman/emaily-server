031029dcca9389ad29957acbdf4d03a487be5b48
Zeyad Etman <zeyadetman@gmail.com>
2020-03-27 22:49:40 +0200

    updates

--- models/Survey.js
+++ models/Survey.js
@@ -6,7 +6,9 @@ const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [RecipientSchema],
+  recipients: {
+    type: [RecipientSchema]
+  },
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
   _user: { type: Schema.Types.ObjectId, ref: "User" },
